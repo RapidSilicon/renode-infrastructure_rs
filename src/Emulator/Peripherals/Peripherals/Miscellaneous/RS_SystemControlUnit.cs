492fa3c3f6d7430a83d7050b5ec18882b71908ea
José Fernando Morales Vargas <jose.morales@rapidsilicon.com>
2024-01-22 22:47:30 -0600

    add SCU GPIo-related enums

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/RS_SystemControlUnit.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/RS_SystemControlUnit.cs
@@ -5,12 +5,14 @@ using Antmicro.Renode.Peripherals.CPU;
 
 namespace Antmicro.Renode.Peripherals.Miscellaneous
 {
-    public class RS_SystemControlUnit<T> : BasicDoubleWordPeripheral, IKnownSize
+    public class RS_SystemControlUnit<T> : BasicDoubleWordPeripheral, IKnownSize, IGPIOReceiver
         where T : BaseCPU, ICPUWithNMI
     {
-        public RS_SystemControlUnit(IMachine machine,long version, T bcpu, T acpu) : base(machine)
+        public RS_SystemControlUnit(IMachine machine,long version, T bcpu = null, T acpu= null) : base(machine)
         {
             this.version = version;
+            this.bcpu = bcpu;
+            this.acpu = acpu;
         }
         private void DefineRegisters()
         {
@@ -19,6 +21,21 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
             {
                 {(long)Registers.IdRev, new DoubleWordRegister(this, 0x10475253)},
             };
+        }
+
+        public void OnGPIO(int number, bool value)
+        {
+            switch((InputPort)number){
+                case InputPort.AcpuWdt:
+
+                    break;
+                case InputPort.BcpuWdt:
+                    break;
+                default:
+                    break;
+            }
+            
+
         }
         private enum Registers : long
         {
@@ -61,5 +78,112 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
         }
         public long Size => 0x3FFF;
         private long version;
+        private T bcpu;
+        private T acpu;
+
+        public enum InputPort: int{
+            
+            Bootstrap0 = 0x0,
+            Bootstrap1 = 0x1,
+            BcpuWdt = 0x2,
+            AcpuWdt = 0x3,
+            Irq1 = 0x4,
+            Irq2 = 0x5,
+            Irq3 = 0x6,
+            Irq4 = 0x7,
+            Irq5 = 0x8,
+            Irq6 = 0x9,
+            Irq7 = 0x10,
+            Irq8 = 0x11,
+            Irq9 = 0x12,
+            Irq10 = 0x13,
+            Irq11 = 0x14,
+            Irq12 = 0x15,
+            Irq13 = 0x16,
+            Irq14 = 0x17,
+            Irq15 = 0x18,
+            Irq16 = 0x19,
+            Irq17 = 0x20,
+            Irq18 = 0x21,
+            Irq19 = 0x22,
+            Irq20 = 0x23,
+            Irq21 = 0x24,
+            Irq22 = 0x25,
+            Irq23 = 0x26,
+            Irq24 = 0x27,
+            Irq25 = 0x28,
+            Irq26 = 0x29,
+            Irq27 = 0x30,
+            Irq28 = 0x31,
+            Irq29 = 0x32,
+            Irq30 = 0x33,
+            Irq31 = 0x34
+        }
+
+        // WDTs not covered in output ports as NMI is called direcly via the OnNMI function 
+        public enum OutputPorts: long{
+            BcpuIrq1,
+            BcpuIrq2,
+            BcpuIrq3,
+            BcpuIrq4,
+            BcpuIrq5,
+            BcpuIrq6,
+            BcpuIrq7,
+            BcpuIrq8,
+            BcpuIrq9,
+            BcpuIrq10,
+            BcpuIrq11,
+            BcpuIrq12,
+            BcpuIrq13,
+            BcpuIrq14,
+            BcpuIrq15,
+            BcpuIrq16,
+            BcpuIrq17,
+            BcpuIrq18,
+            BcpuIrq19,
+            BcpuIrq20,
+            BcpuIrq21,
+            BcpuIrq22,
+            BcpuIrq23,
+            BcpuIrq24,
+            BcpuIrq25,
+            BcpuIrq26,
+            BcpuIrq27,
+            BcpuIrq28,
+            BcpuIrq29,
+            BcpuIrq30,
+            BcpuIrq31,
+            AcpuIrq1,
+            AcpuIrq2,
+            AcpuIrq3,
+            AcpuIrq4,
+            AcpuIrq5,
+            AcpuIrq6,
+            AcpuIrq7,
+            AcpuIrq8,
+            AcpuIrq9,
+            AcpuIrq10,
+            AcpuIrq11,
+            AcpuIrq12,
+            AcpuIrq13,
+            AcpuIrq14,
+            AcpuIrq15,
+            AcpuIrq16,
+            AcpuIrq17,
+            AcpuIrq18,
+            AcpuIrq19,
+            AcpuIrq20,
+            AcpuIrq21,
+            AcpuIrq22,
+            AcpuIrq23,
+            AcpuIrq24,
+            AcpuIrq25,
+            AcpuIrq26,
+            AcpuIrq27,
+            AcpuIrq28,
+            AcpuIrq29,
+            AcpuIrq30,
+            AcpuIrq31
+        }
     }
 }
