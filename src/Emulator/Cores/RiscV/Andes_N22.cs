b6ef4ca391dad6863efdf5ca772dc721554d9709
José Morales <107012016+josfemova-rs@users.noreply.github.com>
2024-01-29 23:09:40 -0600

    Initial Virgo Support (#8)
    * add basic N22 model
    
    * implement andestar pushx CSRs
    
    * base nmi related modifications
    
    * add base SCU model
    
    * add SCU GPIo-related enums
    
    * complete NMI logic for N22 core

---
+++ src/Emulator/Cores/RiscV/Andes_N22.cs
@@ -0,0 +1,136 @@
+﻿//
+// Copyright (c) 2023 Rapid Silicon
+//
+//  This file is licensed under the MIT License.
+//  Full license text is available in 'licenses/MIT.txt'.
+//
+using System;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Core.Structure;
+using Antmicro.Renode.Logging;
+using Antmicro.Renode.Peripherals.Timers;
+using Antmicro.Renode.Utilities;
+using Antmicro.Renode.Utilities.Binding;
+using Endianess = ELFSharp.ELF.Endianess;
+
+namespace Antmicro.Renode.Peripherals.CPU
+{
+    public class Andes_N22 : RiscV32
+    {
+
+        public Andes_N22(IMachine machine, IRiscVTimeProvider timeProvider = null, string cpuType = "rv32imac_zicsr", uint hartId = 0, uint resetVectorAddress = 0x0)
+            : base(timeProvider: timeProvider, cpuType: cpuType, machine: machine, hartId: hartId, allowUnalignedAccesses: true, nmiVectorLength: 1, nmiVectorAddress: resetVectorAddress)
+        {
+            this.resetVectorAddress = resetVectorAddress;
+            MXSTATUS = RegisterValue.Create(0, 32);
+            MMISC_CTL = RegisterValue.Create(0, 32);
+
+            AndestarV5CSR[] notImplementedRWCSRs = {
+                AndestarV5CSR.MTVT_RW, AndestarV5CSR.MDLMB_RW,
+                AndestarV5CSR.MXSTATUS_RW, AndestarV5CSR.MPFT_CTL_RW,
+                AndestarV5CSR.MHSP_CTL_RW, AndestarV5CSR.MCACHE_CTL_RW,
+                AndestarV5CSR.MIRQ_ENTRY_RW, AndestarV5CSR.MINTSEL_JAL,
+                AndestarV5CSR.UITB_RW, AndestarV5CSR.UCODE_RW};
+            AndestarV5CSR[] notImplementedROCSRs = { AndestarV5CSR.MMSC_CFG_RO };
+            foreach (AndestarV5CSR csr in notImplementedRWCSRs)
+            {
+                RegisterCSR((ulong)csr,
+                    () => LogUnhandledCSRRead(csr.ToString()),
+                    val => LogUnhandledCSRWrite(csr.ToString(), val));
+            }
+            foreach (AndestarV5CSR csr in notImplementedROCSRs)
+            {
+                RegisterCSR((ulong)csr,
+                    () => LogUnhandledCSRRead(csr.ToString()),
+                    val => LogWriteOnReadOnlyCSR(csr.ToString(), val));
+            }
+            RegisterCSR((ulong)AndestarV5CSR.PUSHMEPC_RW, () => 0u, GeneratePushCSRWrite(MEPC));
+            RegisterCSR((ulong)AndestarV5CSR.PUSHMCAUSE_RW, () => 0u, GeneratePushCSRWrite(MCAUSE));
+            RegisterCSR((ulong)AndestarV5CSR.PUSHMXSTATUS_RW, () => 0u, GeneratePushCSRWrite(MXSTATUS));
+            RegisterCSR((ulong)AndestarV5CSR.MNVEC_RO, () => NMIVectorAddress.Value, val => LogWriteOnReadOnlyCSR("MNVEC_RO", val));
+            RegisterCSR((ulong)AndestarV5CSR.MMISC_CTL_RW, () => MMISC_CTL, (w) =>
+            {
+                // For now, only support special logic for when NMI is modified
+                MMISC_CTL = w;
+            });
+
+        }
+        public override void Reset()
+        {
+            base.Reset();
+            PC = resetVectorAddress;
+        }
+
+        private Action<ulong> GeneratePushCSRWrite(RegisterValue register)
+        {
+            Action<ulong> result = (write_val) =>
+            {
+                ulong opcode = (ulong)Bus.ReadDoubleWord(PC.RawValue, context: this);
+                var func3 = BitHelper.GetValue(opcode, 12, 3);
+                if (func3 != 0b101)
+                {
+                    this.Log(LogLevel.Error, "Operation not implemented for CSR");
+                    return;
+                }
+                var uimm = write_val;
+                var addr = SP + (uimm << 2);
+                Bus.WriteDoubleWord(addr, register, this);
+            };
+            return result;
+        }
+
+        private ulong LogUnhandledCSRRead(string name)
+        {
+            this.Log(LogLevel.Error, "Reading from an unsupported CSR {0}", name);
+            return 0u;
+        }
+
+        private void LogUnhandledCSRWrite(string name, ulong value)
+        {
+            this.Log(LogLevel.Error, "Writing to an unsupported CSR {0} value: 0x{1:X}", name, value);
+        }
+        private void LogWriteOnReadOnlyCSR(string name, ulong value)
+        {
+            this.Log(LogLevel.Error, "Writing to RO CSR {0} value: 0x{1:X}", name, value);
+        }
+
+        public override void OnNMI(int number, bool value, ulong? mcause = null)
+        {
+
+            bool newNmi = BitHelper.IsBitSet(MMISC_CTL, 9);
+            if (newNmi)
+            {
+                NMIVectorAddress = MTVEC & 0xFFFFFFFC;
+            }
+            else
+            {
+                NMIVectorAddress = resetVectorAddress;
+            }
+            base.OnNMI(number, value, newNmi ? 0xFFFUL : 1);
+        }
+
+        private RegisterValue MXSTATUS;
+        private RegisterValue MMISC_CTL;
+        private readonly uint resetVectorAddress;
+
+        private enum AndestarV5CSR
+        {
+            MTVT_RW = 0x307,
+            MDLMB_RW = 0x7c1,
+            MNVEC_RO = 0x7c3,
+            MXSTATUS_RW = 0x7c4,
+            MPFT_CTL_RW = 0x7c5,
+            MHSP_CTL_RW = 0x7c6,
+            MCACHE_CTL_RW = 0x7ca,
+            MMISC_CTL_RW = 0x7d0,
+            PUSHMXSTATUS_RW = 0x7eb, // Hook to CSRRWI
+            MIRQ_ENTRY_RW = 0x7ec,
+            MINTSEL_JAL = 0x7ed, //Should be in the CLIC
+            PUSHMCAUSE_RW = 0x7ee, // Hook to CSRRWI
+            PUSHMEPC_RW = 0x7ef, // Hook to CSRRWI
+            UITB_RW = 0x800,
+            UCODE_RW = 0x801,
+            MMSC_CFG_RO = 0xFC2
+        }
+    }
+}
---
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/RS_SystemControlUnit.cs
@@ -0,0 +1,187 @@
+using System.Collections.Generic;
+using Antmicro.Renode.Core;
+using Antmicro.Renode.Core.Structure.Registers;
+using Antmicro.Renode.Peripherals.CPU;
+
+namespace Antmicro.Renode.Peripherals.Miscellaneous
+{
+    public class RS_SystemControlUnit : BasicDoubleWordPeripheral, IKnownSize, IGPIOReceiver
+    {
+        public RS_SystemControlUnit(IMachine machine,long version, ICPUWithNMI bcpu = null, ICPUWithNMI acpu= null) : base(machine)
+        {
+            this.version = version;
+            this.bcpu = bcpu;
+            this.acpu = acpu;
+        }
+        private void DefineRegisters()
+        {
+            long idrevReset = (version == (long)RS_SystemControlUnitVersion.Gemini) ? 0x10475253 : 0x10565253;
+            var registersMap = new Dictionary<long, DoubleWordRegister>
+            {
+                {(long)Registers.IdRev, new DoubleWordRegister(this, 0x10475253)},
+            };
+        }
+
+        public void OnGPIO(int number, bool value)
+        {
+            switch((InputPort)number){
+                case InputPort.AcpuWdt:
+
+                    break;
+                case InputPort.BcpuWdt:
+                    bcpu.OnNMI(0, value, null);
+                    break;
+                default:
+                    break;
+            }
+        }
+        private enum Registers : long
+        {
+            IdRev = 0x0,
+            SoftwareResetConttrol = 0x4,
+            PllConfig0 = 0x8,
+            PllConfig1 = 0xC,
+            PllConfig2 = 0x10,
+            PllConfig3 = 0x14,
+            PllConfig4 = 0x18,
+            PllStatus = 0x1C,
+            DividerControl = 0x20,
+            GatingControl = 0x24,
+            DebugControl = 0x28,
+            IrqAcpuBcpuWdtMask = 0x2C,
+            IrqMaskMapControl_n = 0x30, // n in [1,31]
+            IsolationControl = 0xAC,
+            BootstrapStatus = 0xB0,
+            MainDividerControl = 0xB4,
+            PufccControl = 0xB8,
+            UsbControl = 0xBC,
+            FpgaPll = 0xC0,
+            WdtPause = 0xC4,
+            DDRMode = 0xC8,
+            OscillatorControl = 0xCC,
+            GptPause = 0xD0,
+            SramControl0 = 0xD4,
+            SramControl1 = 0xD8,
+            AcpuResetVector = 0xDC,
+            AcpuMemoryMargin = 0xE0,
+            MemorySubSystemMemoryMargin = 0xE4,
+            ConfigSubSystemMemoryMargin = 0xE8,
+            PadsModeControlSlewRateControl = 0xEC,
+            SpareReg = 0xF0
+        }
+        public enum RS_SystemControlUnitVersion : long
+        {
+            Gemini = 0x0,
+            Virgo = 0x1
+        }
+        public long Size => 0x3FFF;
+        private long version;
+        private ICPUWithNMI bcpu;
+        private ICPUWithNMI acpu;
+
+        public enum InputPort: int{
+            
+            Bootstrap0 = 0x0,
+            Bootstrap1 = 0x1,
+            BcpuWdt = 0x2,
+            AcpuWdt = 0x3,
+            Irq1 = 0x4,
+            Irq2 = 0x5,
+            Irq3 = 0x6,
+            Irq4 = 0x7,
+            Irq5 = 0x8,
+            Irq6 = 0x9,
+            Irq7 = 0x10,
+            Irq8 = 0x11,
+            Irq9 = 0x12,
+            Irq10 = 0x13,
+            Irq11 = 0x14,
+            Irq12 = 0x15,
+            Irq13 = 0x16,
+            Irq14 = 0x17,
+            Irq15 = 0x18,
+            Irq16 = 0x19,
+            Irq17 = 0x20,
+            Irq18 = 0x21,
+            Irq19 = 0x22,
+            Irq20 = 0x23,
+            Irq21 = 0x24,
+            Irq22 = 0x25,
+            Irq23 = 0x26,
+            Irq24 = 0x27,
+            Irq25 = 0x28,
+            Irq26 = 0x29,
+            Irq27 = 0x30,
+            Irq28 = 0x31,
+            Irq29 = 0x32,
+            Irq30 = 0x33,
+            Irq31 = 0x34
+        }
+
+        // WDTs not covered in output ports as NMI is called direcly via the OnNMI function 
+        public enum OutputPorts: long{
+            BcpuIrq1,
+            BcpuIrq2,
+            BcpuIrq3,
+            BcpuIrq4,
+            BcpuIrq5,
+            BcpuIrq6,
+            BcpuIrq7,
+            BcpuIrq8,
+            BcpuIrq9,
+            BcpuIrq10,
+            BcpuIrq11,
+            BcpuIrq12,
+            BcpuIrq13,
+            BcpuIrq14,
+            BcpuIrq15,
+            BcpuIrq16,
+            BcpuIrq17,
+            BcpuIrq18,
+            BcpuIrq19,
+            BcpuIrq20,
+            BcpuIrq21,
+            BcpuIrq22,
+            BcpuIrq23,
+            BcpuIrq24,
+            BcpuIrq25,
+            BcpuIrq26,
+            BcpuIrq27,
+            BcpuIrq28,
+            BcpuIrq29,
+            BcpuIrq30,
+            BcpuIrq31,
+            AcpuIrq1,
+            AcpuIrq2,
+            AcpuIrq3,
+            AcpuIrq4,
+            AcpuIrq5,
+            AcpuIrq6,
+            AcpuIrq7,
+            AcpuIrq8,
+            AcpuIrq9,
+            AcpuIrq10,
+            AcpuIrq11,
+            AcpuIrq12,
+            AcpuIrq13,
+            AcpuIrq14,
+            AcpuIrq15,
+            AcpuIrq16,
+            AcpuIrq17,
+            AcpuIrq18,
+            AcpuIrq19,
+            AcpuIrq20,
+            AcpuIrq21,
+            AcpuIrq22,
+            AcpuIrq23,
+            AcpuIrq24,
+            AcpuIrq25,
+            AcpuIrq26,
+            AcpuIrq27,
+            AcpuIrq28,
+            AcpuIrq29,
+            AcpuIrq30,
+            AcpuIrq31
+        }
+    }
+}
