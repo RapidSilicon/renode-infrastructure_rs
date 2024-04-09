2ef3b503e7cd25809d4ca600130492e458a5fedd
Maleeha <maleeha.akbar@rapidsilicon.com>
2024-04-09 08:36:42 +0500

    update write logic

--- src/Emulator/Main/Peripherals/Memory/MappedMemory.cs
+++ src/Emulator/Main/Peripherals/Memory/MappedMemory.cs
@@ -299,9 +299,9 @@ namespace Antmicro.Renode.Peripherals.Memory
                 var segment = segments[GetSegmentNo(currentOffset)];
                 var length = Math.Min(count - written, (int)(SegmentSize - localOffset));
                // Marshal.Copy(array, startingIndex + written, new IntPtr(segment.ToInt64() + localOffset), length);
-                Marshal.Copy(array, startingIndex - written, new IntPtr(segment.ToInt64() + localOffset), length);
+                Marshal.Copy(array, startingIndex - written, new IntPtr(segment.ToInt64() - localOffset), length);
                // written += length;
-               written += length;
+               written -= length;
 
                 InvalidateMemoryFragment(currentOffset, length);
             }
