54632ad714d7749293b221e2b37a794403194a4e
Maleeha <maleeha.akbar@rapidsilicon.com>
2024-04-23 00:56:14 +0500

    remove warning

--- src/Emulator/Peripherals/Peripherals/Miscellaneous/virgo_pad.cs
+++ src/Emulator/Peripherals/Peripherals/Miscellaneous/virgo_pad.cs
@@ -27,6 +27,7 @@ namespace Antmicro.Renode.Peripherals.Miscellaneous
            // IRQ = new GPIO();
             //irqManager = new GPIOInterruptManager(IRQ, State);
          RegistersCollection = new DoubleWordRegisterCollection(this);
+          iomode = new iomode[NumberOfPins];
             PrepareRegisters();
         }
        
